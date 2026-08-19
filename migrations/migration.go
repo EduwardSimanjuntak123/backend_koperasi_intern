a510b73475e5bdac15e4f42478737b8d694359a0
EduwardSimanjuntak123 <149449338+EduwardSimanjuntak123@users.noreply.github.com>
2026-08-19 13:38:31 +0700

    del rollback auto

--- migrations/migration.go
+++ migrations/migration.go
@@ -3,80 +3,26 @@ package migrations
 import (
 	"log"
 
-	"backend_koperasi/internal/models"
-
 	"gorm.io/gorm"
+
+	"backend_koperasi/internal/models"
 )
 
-// Run menjalankan seluruh migration database.
-// Digunakan saat aplikasi pertama kali dijalankan
-// atau ketika ada penambahan struktur tabel.
 func Run(db *gorm.DB) error {
+	err := db.AutoMigrate(
 
-	// =====================================================
-	// DEVELOPMENT ONLY
-	// =====================================================
-	// Hapus seluruh tabel sebelum membuat ulang.
-	// Jangan digunakan di PRODUCTION karena akan
-	// menghapus seluruh data yang ada.
-	// =====================================================
-	err := db.Migrator().DropTable(
-		&models.Order_Item{},
-		&models.Order{},
-		&models.Cart_Item{},
-		&models.Cart{},
-		&models.Favorite{},
-		&models.Product{},
 		&models.CategoryProduct{},
-		&models.Brand{},
-		&models.Supplier{},
-		&models.Unit{},
-		&models.PaymentMethod{},
-		&models.PointLocation{},
-		&models.StoreMember{},
-		&models.Store{},
-		&models.Roles{},
 		&models.User{},
-	)
-
-	if err != nil {
-		log.Println("Drop table failed:", err)
-		return err
-	}
-
-	// =====================================================
-	// AutoMigrate akan:
-	// - Membuat tabel jika belum ada
-	// - Menambahkan kolom baru jika ada perubahan model
-	// - Tidak menghapus kolom lama
-	// =====================================================
-	err = db.AutoMigrate(
-		&models.Roles{},
-		&models.User{},
-		// ===== Master Data =====
-		&models.Brand{},
-		&models.Supplier{},
-		&models.Unit{},
-		&models.CategoryProduct{},
-
-		// ===== User & Store =====
-
 		&models.Store{},
 		&models.StoreMember{},
-
-		// ===== Product =====
+		&models.Roles{},
 		&models.Product{},
-
-		// ===== Cart =====
 		&models.Cart{},
 		&models.Cart_Item{},
 		&models.Favorite{},
-
-		// ===== Payment =====
 		&models.PointLocation{},
 		&models.PaymentMethod{},
 
-		// ===== Transaction =====
 		&models.Order{},
 		&models.Order_Item{},
 	)
